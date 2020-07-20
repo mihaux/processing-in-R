c0e958e6b94d4589fb1772125a8a4ad24cf32ad3
mihaux <michalzulcinski@gmail.com>
2020-07-20 16:39:26 +0200

    Update run_Modify_cols.R

--- run_Modify_cols.R
+++ run_Modify_cols.R
@@ -9,7 +9,8 @@ if (length(args)!=1) {
        \n(1 - input) path to .csv file with count data", call.=FALSE)
 }
 
-cat("Example of usage: \nRscript run_Modify_cols.R /Users/michal/Documents/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/run_1/featCounts_SE/all_counts_dups_run1_SE.csv")
+# Example of usage: 
+# Rscript run_Modify_cols.R /Users/michal/Documents/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/run_1/featCounts_SE/all_counts_dups_run1_SE.csv
 
 # INPUT examples
 # /Users/michal/Documents/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/run_1/featCounts_SE/all_counts_dups_run1_SE.csv
