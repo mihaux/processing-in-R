7f3a69407d7fb0a27313c32f4fd0f9ee657682b5
Michal <michalzulcinski@gmail.com>
2020-07-20 15:47:24 +0200

    added new file

--- run_PCA_built-in.R
+++ run_PCA_built-in.R
@@ -7,29 +7,73 @@ if (!requireNamespace("limma", quietly = TRUE)) BiocManager::install("limma"); l
 
 #args <- commandArgs(trailingOnly = TRUE)
 
-args <- c("/Users/ummz/OneDrive - University of Leeds/ANALYSES/results_run_IV_Feb20/6_downstream_analysis/paired-end/normalised_cpm.csv",
-          "/Users/ummz/Documents/OneDrive - University of Leeds/data/metadata/cic_clinical_data_v2_split/cic_clinical_data_v2_summary.csv",
-          "/Users/ummz/OneDrive - University of Leeds/ANALYSES/results_run_IV_Feb20/6_downstream_analysis/single-end")
+# create a shortcut for the OneDrive directory where all files are stored
+main_dir <- "/Users/michal/Documents/OneDrive - University of Leeds"      # on my mac
+# main_dir <- "/Users/ummz/OneDrive - University of Leeds"                # on uni mac
 
-cat("Example of usage: \n Rscript downstream.R /Users/ummz/OneDrive - University of Leeds/ANALYSES/results_run_IV_Feb20/5_featureCounts/single-end/processed/mode_II/all_counts_SE.csv /Users/ummz/OneDrive - University of Leeds/ANALYSES/results_run_IV_Feb20/6_downstream_analysis/CiC_Clinical_data_FINAL.csv /Users/ummz/OneDrive - University of Leeds/ANALYSES/results_run_IV_Feb20/6_downstream_analysis/single-end")
+#################################################### INPUT data description #################################################### 
+# there are 2 runs (run_1 & run_2), 2 running modes (SE & PE) within each and 2 options ("dups" & "nodups") within each
+# /run_1/featCounts_SE/all_counts_dups_run1_SE.csv      # /run_1/featCounts_SE/all_counts_nodups_run1_SE.csv
+# /run_1/featCounts_PE/all_counts_dups_run1_PE.csv      # /run_1/featCounts_PE/all_counts_nodups_run1_PE.csv
+
+# /run_2/featCounts_SE/all_counts_dups_run2_SE.csv      # /run_2/featCounts_SE/all_counts_nodups_run2_SE.csv
+# /run_2/featCounts_PE/all_counts_dups_run2_PE.csv      # /run_2/featCounts_PE/all_counts_nodups_run2_PE.csv
+################################################################################################################################
+
+# run for all_counts_dups_run1_SE.csv
 
 if (length(args)!=3) {
   stop("3 arguments must be supplied: 
        \n(1 - input) path to .csv file with count data, 
-       \n(2 - annotation) path to .csv annotation file 
-       \nand (3 - output) path where output files should be stored", call.=FALSE)
+       \n(2 - annotation) path to .csv annotation file and 
+       \n(3 - output) path where output files should be stored", call.=FALSE)
 }
 
-cat("Directories with data (IN): ")
-cat(args[1], sep="\n")
+cat("Example of usage: \nRscript run_PCA_built-in.R /Users/ummz/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/ANALYSES/rerun_FINAL_July20/run_1/featCounts_SE/all_counts_dups_run1_SE.csv /Users/ummz/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/data/metadata/cic_clinical_data_v2_split/cic_clinical_data_v2_summary.csv /Users/ummz/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/ANALYSES/downstream/rerun_FINAL_July20/run_1")              
 
-cat("Directory for results (OUT): ")
-cat(args[3], sep="\n")
+args <- c(paste0(main_dir, "/ANALYSES/rerun_FINAL_July20/run_1/featCounts_SE/all_counts_dups_run1_PE.csv"),
+          paste0(main_dir, "/data/metadata/cic_clinical_data_v2_split/cic_clinical_data_v2_summary.csv"),
+          paste0(main_dir, "/ANALYSES/downstream/rerun_FINAL_July20/run_1"))
 
+cat("Directories with data (IN): "); cat(args[1], sep="\n")
+cat("Directory for results (OUT): "); cat(args[3], sep="\n")
 setwd(args[3])
 
 # load count data
-df <- read.csv(args[1], row.names = 1, header = TRUE)
+df <- read.csv(args[1], row.names = 1, header = TRUE) # data.frame with counts only | cols=
+
+################################################################################################################################
+if(FALSE){
+# run for all datasets
+dir1 <- "/Users/michal/Documents/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/run_1/featCounts_SE/all_counts_dups_run1_SE.csv"
+dir2 <- "/Users/michal/Documents/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/run_1/featCounts_SE/all_counts_nodups_run1_SE.csv"
+
+dir3 <- "/Users/michal/Documents/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/run_1/featCounts_PE/all_counts_dups_run1_PE.csv"
+dir4 <- "/Users/michal/Documents/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/run_1/featCounts_PE/all_counts_nodups_run1_PE.csv"
+
+dir5 <- "/Users/michal/Documents/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/run_2/featCounts_SE/all_counts_dups_run2_SE.csv"
+dir6 <- "/Users/michal/Documents/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/run_2/featCounts_SE/all_counts_nodups_run2_SE.csv"
+
+dir7 <- "/Users/michal/Documents/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/run_2/featCounts_PE/all_counts_dups_run2_PE.csv"
+dir8 <- "/Users/michal/Documents/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/run_2/featCounts_PE/all_counts_nodups_run2_PE.csv"
+list_dir <- list(dir1, dir2, dir3, dir4, dir5, dir6, dir7, dir8)
+
+for (i in list_dir) {
+  print(i)
+  df <- read.csv(i, row.names = 1, header = TRUE)
+  print(df[1:5,1:5])
+  #print(dim(df))
+  #print(head(colnames(df)))
+  #print(head(rownames(df)))
+  cat("############################")
+}
+}
+################################################################################################################################
+
+df <- read.csv(dir1, row.names = 1, header = TRUE)
+# head(rowsnames(df))
+# head(rownames(df))
+cat("############################")
 
 # if running for mode_II then, the colnames need to be changed
 IDs <- sub(".Aligned.sortedByCoord.out.bam*", "", colnames(df))
