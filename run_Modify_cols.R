c577d791b14116e29dff8d159641984f777560f4
mihaux <michalzulcinski@gmail.com>
2020-07-20 16:35:32 +0200

    Update run_Modify_cols.R

--- run_Modify_cols.R
+++ run_Modify_cols.R
@@ -1,17 +1,17 @@
 # this script changes default colnames from "X11026.Aligned.sortedByCoord.out.bam" to "ID_11026"
-# output files have "_mod" added in their names
+# output files have "_mod" added in their names and are placed in the same directory as input files
 
 # install (if necessary) and load package
+if (!requireNamespace("stringr", quietly = TRUE)) install.packages("stringr"); library(stringr)
 
-if (length(args)!=2) {
-  stop("2 arguments must be supplied: 
-       \n(1 - input) path to .csv file with count data and
-       \n(2 - output) path where output files should be stored", call.=FALSE)
+if (length(args)!=1) {
+  stop("1 argument must be supplied: 
+       \n(1 - input) path to .csv file with count data , call.=FALSE)
 }
 
-cat("Example of usage: \nRscript run_Modify_cols.R /Users/michal/Documents/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/run_1/featCounts_SE/all_counts_dups_run1_SE.csv /Users/michal/Documents/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/run_1/featCounts_SE/all_counts_dups_run1_SE_mod.csv
+cat("Example of usage: \nRscript run_Modify_cols.R /Users/michal/Documents/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/run_1/featCounts_SE/all_counts_dups_run1_SE.csv
 
-# INPUT examples
+  # INPUT examples
 # /Users/michal/Documents/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/run_1/featCounts_SE/all_counts_dups_run1_SE.csv
 # /Users/michal/Documents/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/run_1/featCounts_SE/all_counts_nodups_run1_SE.csv
 
@@ -20,9 +20,6 @@ cat("Example of usage: \nRscript run_Modify_cols.R /Users/michal/Documents/OneDr
 
 args <- commandArgs(trailingOnly = TRUE)
 
-cat("Directories with data (IN): "); cat(args[1], sep="\n")
-cat("Directory for results (OUT): "); cat(args[2], sep="\n")
-
 # load count data
 df <- read.csv(args[1], row.names = 1, header = TRUE)
 
@@ -30,7 +27,7 @@ df <- read.csv(args[1], row.names = 1, header = TRUE)
 IDs <- sub(".Aligned.sortedByCoord.out.bam*", "", colnames(df))
 IDs_final <- sub("X*", "ID_", IDs)
 colnames(df) <- IDs_final
-
-# write modified count tables as .csv
-write.csv(df, ".csv")
+       
+# write modified count tables as .csv with "_mod" added in the new file name
+write.csv(df, str_replace(args[1], ".csv", "_mod.csv"))
 
