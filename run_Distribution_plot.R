c6e5043c9d2f1876d34a6911dbd083f7f3e52321
mihaux <michalzulcinski@gmail.com>
2020-07-20 18:45:35 +0200

    Update run_Distribution_plot.R

---
+++ run_Distribution_plot.R
@@ -0,0 +1,35 @@
+# this script plots distribution plots of counts
+# output files 
+
+# install (if necessary) and load package
+
+args <- commandArgs(trailingOnly = TRUE)
+
+if (length(args)!=1) {
+  stop("1 argument must be supplied: 
+       \n(1 - input) path to .csv file with count data", call.=FALSE)
+}
+
+# Example of usage: 
+# Rscript run_Modify_cols.R /Users/michal/Documents/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/run_1/featCounts_SE/all_counts_dups_run1_SE.csv
+
+# INPUT examples
+# /Users/michal/Documents/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/run_1/featCounts_SE/all_counts_dups_run1_SE.csv
+# /Users/michal/Documents/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/run_1/featCounts_SE/all_counts_nodups_run1_SE.csv
+
+# OUTPUT examples
+# /Users/michal/Documents/OneDrive - University of Leeds/ANALYSES/rerun_FINAL_July20/run_1/featCounts_SE/all_counts_dups_run1_SE_mod.csv
+
+# load count data
+df <- read.csv(args[1], row.names = 1, header = TRUE)
+
+# if running for mode_II then, the colnames need to be changed
+IDs <- sub(".Aligned.sortedByCoord.out.bam*", "", colnames(df))
+IDs_final <- sub("X*", "ID_", IDs)
+colnames(df) <- IDs_final
+       
+# write modified count tables as .csv with "_mod" added in the new file name
+write.csv(df, str_replace(args[1], ".csv", "_mod.csv"))
+
+cat("Finished!", "\nCreated:", str_replace(args[1], ".csv", "_mod.csv"))
+
